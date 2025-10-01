10bc5cdffb7e85cda2e99f12c34ea442ff5579b2
Brian Redfern <brianwredfern@gmail.com>
2025-10-01 12:27:00 -0700

    total refactor into a vanilla js app, with midi support coming soon

--- test/Synth.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Synth from '@/components/Synth.vue'
-
-describe('Synth', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Synth)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
